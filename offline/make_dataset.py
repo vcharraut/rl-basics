8119a4e1822a943be6309f8f0e8bbec7cc719ba0
Valentin Charraut <valentin.charraut@protonmail.com>
2023-10-11 10:30:46 +0200

    Update make dataset

--- offline/make_dataset.py
+++ offline/make_dataset.py
@@ -6,10 +6,11 @@ from stable_baselines3 import SAC
 if __name__ == "__main__":
     env_name = "HalfCheetah-v4"
     run_dir = f"logs/{env_name}"
+    seed = 42
 
     env = DataCollectorV0(gym.make(env_name))
 
-    model = SAC("MlpPolicy", env, tensorboard_log=run_dir)
+    model = SAC("MlpPolicy", env, buffer_size=100_000, learning_starts=25_000, tensorboard_log=run_dir, seed=seed)
     model.learn(total_timesteps=1_000_000, progress_bar=True)
 
     dataset = create_dataset_from_collector_env(
